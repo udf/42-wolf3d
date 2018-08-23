3fcf38081fadaeec4ad7280ff375a3ac58893e7b
udf <tabhooked@gmail.com>
2018-08-23 13:43:47 +0200

    Render alpha channel on doors

--- srcs/view_render.c
+++ srcs/view_render.c
@@ -6,7 +6,7 @@
 /*   By: mhoosen <mhoosen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/14 19:41:24 by mhoosen           #+#    #+#             */
-/*   Updated: 2018/08/23 13:28:19 by mhoosen          ###   ########.fr       */
+/*   Updated: 2018/08/23 13:41:16 by mhoosen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ static void		draw_column(t_view_data *v, int screen_x, t_hit hit, float dist)
 	t_frange	yr;
 	float		y;
 	int			t_y;
+	t_pixel		pix;
 
 	yr.s = ((float)v->h - y_len) / 2.0f - y_len * hit.v_shift;
 	yr.e = ((float)v->h + y_len) / 2.0f - y_len * hit.v_shift;
@@ -53,11 +54,26 @@ static void		draw_column(t_view_data *v, int screen_x, t_hit hit, float dist)
 	while (y <= MIN(yr.e, (float)v->h) - 1)
 	{
 		t_y = iroundf(ft_fmapf(y, yr, (t_frange){1, (float)hit.tex->h}) - 1);
-		*buf_pixel(&v->buf, screen_x, iroundf(y)) =
-			*((Uint32 *)&hit.tex->data[hit.tex_x + t_y * hit.tex->h]);
+		pix = hit.tex->data[hit.tex_x + t_y * hit.tex->h];
+		if (pix.a == 255)
+			*buf_pixel(&v->buf, screen_x, iroundf(y)) = *((Uint32 *)&pix);
 		v->z_column[iroundf(y)] = dist;
 		y++;
 	}
+}
+
+static int		hit_has_alpha(t_hit	hit)
+{
+	int y;
+
+	y = 0;
+	while (y < hit.tex->h)
+	{
+		if (hit.tex->data[hit.tex_x + y * hit.tex->h].a != 255)
+			return (1);
+		y++;
+	}
+	return (0);
 }
 
 static void		draw_cast(t_view_data *v, const t_model_data *m, int x, t_ray r)
@@ -68,15 +84,16 @@ static void		draw_cast(t_view_data *v, const t_model_data *m, int x, t_ray r)
 
 	p_d = (t_p2d){modff(r.p.x, &p_t.x), modff(r.p.y, &p_t.y)};
 	hit = view_raycast(r.a, p_t, p_d);
-	if (!hit.valid || !hit.tex)
-		return ;
-	if (hit.v_shift > 0.0f) // TODO: check for texture transparency
+	if (hit.valid && hit.tex)
 	{
-		hit.pos.x += cos_deg(r.a) * 0.0001f;
-		hit.pos.y += sin_deg(r.a) * 0.0001f;
-		draw_cast(v, m, x, (t_ray){hit.pos, r.a});
+		if (hit.v_shift > 0.0f || hit_has_alpha(hit))
+		{
+			hit.pos.x += cos_deg(r.a) * 0.0001f;
+			hit.pos.y += sin_deg(r.a) * 0.0001f;
+			draw_cast(v, m, x, (t_ray){hit.pos, r.a});
+		}
+		draw_column(v, x, hit, ray_dist(m->me.rot, m->me.pos, hit.pos));
 	}
-	draw_column(v, x, hit, ray_dist(m->me.rot, m->me.pos, hit.pos));
 	view_sprites_draw_column(v, x, r);
 }
 
@@ -87,6 +104,8 @@ void			view_render_main(t_view_data *v, const t_model_data *m)
 	t_ray	ray;
 
 	v->fov_r = make_fov_range(m->me.rot, v->fov);
+	// view_raycast(359.999969f, (t_p2d){15, 12}, (t_p2d){0.999999f, 0.999999f});
+	// exit(0);
 	view_sprites_compute(v, m);
 	x = 0;
 	while (x < v->w)
