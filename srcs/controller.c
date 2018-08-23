4406c088e2b262b670a696e2c9e184cc28139c81
udf <tabhooked@gmail.com>
2018-08-23 16:25:52 +0200

    drop keys on key down

--- srcs/controller.c
+++ srcs/controller.c
@@ -6,7 +6,7 @@
 /*   By: mhoosen <mhoosen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/14 18:24:21 by mhoosen           #+#    #+#             */
-/*   Updated: 2018/08/15 00:20:26 by mhoosen          ###   ########.fr       */
+/*   Updated: 2018/08/23 16:25:31 by mhoosen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,6 @@ static void	process_input(float secs)
 			dir *= -1.0f;
 		model_player_rotate(secs * dir * 90.f);
 	}
-	if (state[SDL_SCANCODE_Z])
-		model_player_drop_key(1);
-	if (state[SDL_SCANCODE_X])
-		model_player_drop_key(2);
 }
 
 int			controller_process_events(float elapsed_secs)
@@ -42,9 +38,18 @@ int			controller_process_events(float elapsed_secs)
 	SDL_Event event;
 
 	while (SDL_PollEvent(&event))
+	{
 		if (event.type == SDL_QUIT ||
 			(event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_ESCAPE))
 			return (1);
+		if (event.type == SDL_KEYDOWN)
+		{
+			if (event.key.keysym.scancode == SDL_SCANCODE_Z)
+				model_player_drop_key(1);
+			else if (event.key.keysym.scancode == SDL_SCANCODE_X)
+				model_player_drop_key(2);
+		}
+	}
 	process_input(elapsed_secs);
 	return (0);
 }
